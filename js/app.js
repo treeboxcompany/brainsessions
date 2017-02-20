12cb87c01df1809cac094449f2300378dc6bac39
Andres Ospina <andressospina@gmail.com>
2017-02-20 07:37:38 -0500

    Page speed optimized
    Se optimizo el tiempo de carga del home page.

--- js/app.js
+++ js/app.js
@@ -2,3 +2,5 @@ $(document).foundation()
 $(document).ready(function(){
   $('.bxslider').bxSlider();
   });
+$(document).on('click', '[data-lightbox]', lity);
+new WOW().init();
--- readme.txt
+++
@@ -1,4 +0,0 @@
-## Brain Sessions ##
-Este es el proyecto de educación online de treebox.
-
-Evidentemente el pitch esta pendiente.
